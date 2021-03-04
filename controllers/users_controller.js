fbbfaa4cdfa0d2746793dc96985a4c1dae491554
Pranav <pranavmendi@gmail.com>
2021-03-05 00:09:06 +0530

    Update routes in sign-up/in form actions and add controllers for them

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,19 +1,29 @@
+// render profile page
 module.exports.profile = (req, res) => {
   return res.end("<h1>User Profile</h1>");
 };
 
+// render posts
 module.exports.posts = (req, res) => {
   return res.end("<h1>User Posts</h1>");
 };
 
+// render sign-up page
 module.exports.signUp = (req, res) => {
   return res.render("user_sign_up", {
     title: "Sign Up",
   });
 };
 
+// render sign-in page
 module.exports.signIn = (req, res) => {
   return res.render("user_sign_in", {
     title: "Sign In",
   });
 };
+
+// get sign-up data
+moduel.exports.create = (req, res) => {};
+
+// sign-in and create session for the user
+moduel.exports.signIn = (req, res) => {};
