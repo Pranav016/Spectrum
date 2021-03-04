8860074d6d47e046b4c953db0874ba7b3116ddd9
Pranav <pranavmendi@gmail.com>
2021-03-05 03:54:33 +0530

    Add cookie-parser and mongoose to setup middleware and connection to MongoDB

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
 const app = express();
 const port = 8000;
+const db = require("./config/mongoose");
 
 // routes
 const homeRoute = require("./routes"); //by default requires index file
 
-// using middlewares
+// setting up middlewares
+app.use(express.urlencoded({ extended: true }));
+/*This method is inbuilt in express to recognize
+the incoming Request Object as strings or arrays*/
+app.use(express.static("./assets"));
+app.use(cookieParser());
+
 app.use("/", homeRoute);
 
 // setup view engine
 app.set("view engine", "ejs");
 app.set("views", "./views");
 
+// starting the server
 app.listen(port, function (err) {
   if (err) {
     console.log(`Error: ${err}`);
