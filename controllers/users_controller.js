4560bb9675f45f9785f032e0f30855c67f4448ba
Pranav <pranavmendi@gmail.com>
2021-03-05 00:03:02 +0530

    Add routers and controllers for sign-up and sign-in page

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -5,3 +5,15 @@ module.exports.profile = (req, res) => {
 module.exports.posts = (req, res) => {
   return res.end("<h1>User Posts</h1>");
 };
+
+module.exports.signUp = (req, res) => {
+  return res.render("user_sign_up", {
+    title: "Sign Up",
+  });
+};
+
+module.exports.signIn = (req, res) => {
+  return res.render("user_sign_in", {
+    title: "Sign In",
+  });
+};
